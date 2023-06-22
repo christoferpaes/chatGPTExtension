ef1d16b5975f00eb2e5f35f6740370973b1ec798
Christofer Patrick Paes <49449786+christoferpaes@users.noreply.github.com>
2023-06-22 11:44:02 -0700

    Create content.js

---
+++ content.js
@@ -0,0 +1,50 @@
+// content.js
+
+// Function to extract the math equation from the webpage
+function extractMathEquation() {
+  // Add your parsing logic here to extract the math equation from the webpage
+  // You can use the provided HTML structure to guide your parsing process
+  // Example:
+  const mathEquationElement = document.querySelector('.questionwrap.questionpane .MathJax-Element-14-Frame');
+  const mathEquation = mathEquationElement.getAttribute('data-mathml');
+  return mathEquation;
+}
+
+// Function to handle the response from ChatGPT
+function handleChatGPTResponse(response) {
+  // Process the response from ChatGPT and perform any necessary actions
+  // Example:
+  const generatedText = response.choices[0].text;
+  console.log('Generated Text:', generatedText);
+  // Perform further actions with the generated text as needed
+}
+
+// Main function to initiate the process
+function processWebpage() {
+  const mathEquation = extractMathEquation();
+  
+  // Use the extracted math equation as input for ChatGPT API
+  // Example:
+  const apiUrl = 'https://api.openai.com/v1/engines/davinci-codex/completions';
+  const prompt = `Given the math equation: ${mathEquation}`;
+  const data = {
+    prompt,
+    max_tokens: 50
+  };
+  
+  // Send a request to the ChatGPT API
+  fetch(apiUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer YOUr_API_KEY'  // Replace with your actual API key
+    },
+    body: JSON.stringify(data)
+  })
+  .then(response => response.json())
+  .then(handleChatGPTResponse)
+  .catch(error => console.error('Error:', error));
+}
+
+// Call the main function to start the process when the webpage is loaded
+processWebpage();
