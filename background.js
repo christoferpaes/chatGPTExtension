ba4ba0762d15f87bfbb53b6794365a4774c4230c
Christofer Patrick Paes <49449786+christoferpaes@users.noreply.github.com>
2023-06-22 12:38:48 -0700

    Update background.js

--- background.js
+++ background.js
@@ -1,21 +1,29 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.message === "math_equation") {
-    sendToChatGPT(request.equation, function(response) {
-      sendResponse({ message: "chat_response", response: response });
+// background.js
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.message === 'math_equation') {
+    // Send the math equation to ChatGPT and handle the response
+    sendToChatGPT(request.equation, function (response) {
+      // Send the response back to the content script
+      sendResponse({ message: 'chat_response', response: response });
     });
+
+    // Return true to indicate that the response will be sent asynchronously
     return true;
   }
 });
 
 function sendToChatGPT(equation, callback) {
-  const apiKey = "YOUR_OPENAI_API_KEY"; // replace with your API key 
-  const apiUrl = "https://api.openai.com/v1/engines/davinci-codex/completions";
+  // Make the API request to ChatGPT using equation
+  // Replace 'YOUR_OPENAI_API_KEY' with your actual OpenAI API key
+  const apiKey = 'YOUR_OPENAI_API_KEY';
+  const apiUrl = 'https://api.openai.com/v1/engines/davinci-codex/completions';
 
   fetch(apiUrl, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${apiKey}`
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${apiKey}`
     },
     body: JSON.stringify({
       prompt: equation,
@@ -28,7 +36,8 @@ function sendToChatGPT(equation, callback) {
       callback(chatResponse);
     })
     .catch(error => {
-      console.error("Error:", error);
-      callback("An error occurred while processing the request.");
+      console.error('Error:', error);
+      callback('An error occurred while processing the request.');
     });
 }
+
