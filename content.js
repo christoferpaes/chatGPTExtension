aa5ee8ca1b6795abc823f01bcfede4b3dfc255a8
Christofer Patrick Paes <49449786+christoferpaes@users.noreply.github.com>
2023-06-22 12:38:37 -0700

    Update content.js

--- content.js
+++ content.js
@@ -1,31 +1,51 @@
+// content.js
+
+// Function to extract the math equation from the webpage
 function extractMathEquation() {
-  const mathEquationElement = document.querySelector(".questionwrap.questionpane .MathJax-Element-14-Frame");
-  const mathEquation = mathEquationElement.getAttribute("data-mathml");
+  // Add your parsing logic here to extract the math equation from the webpage
+  // You can use the provided HTML structure to guide your parsing process
+  // Example:
+  const mathEquationElement = document.querySelector('.math-equation-selector');
+  const mathEquation = mathEquationElement.innerText;
   return mathEquation;
 }
 
-function sendMessageToBackgroundScript(message, data, callback) {
-  chrome.runtime.sendMessage({ message, data }, response => {
-    if (chrome.runtime.lastError) {
-      console.error(chrome.runtime.lastError);
-      callback("");
-    } else {
-      callback(response);
-    }
-  });
-}
-
+// Function to handle the response from ChatGPT
 function handleChatGPTResponse(response) {
-  const generatedText = response;
-  console.log("Generated Text:", generatedText);
+  // Process the response from ChatGPT and perform any necessary actions
+  // Example:
+  const generatedText = response.choices[0].text;
+  console.log('Generated Text:', generatedText);
   // Perform further actions with the generated text as needed
 }
 
+// Main function to initiate the process
 function processWebpage() {
   const mathEquation = extractMathEquation();
-  const prompt = `Given the math equation: ${mathEquation}`;
 
-  sendMessageToBackgroundScript("math_equation", prompt, handleChatGPTResponse);
+  // Use the extracted math equation as input for ChatGPT API
+  // Example:
+  const apiUrl = 'https://api.openai.com/v1/engines/davinci-codex/completions';
+  const prompt = `Given the math equation: ${mathEquation}`;
+  const data = {
+    prompt,
+    max_tokens: 50
+  };
+
+  // Send a request to the ChatGPT API
+  fetch(apiUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer YOUR_API_KEY'  // Replace with your actual API key
+    },
+    body: JSON.stringify(data)
+  })
+    .then(response => response.json())
+    .then(handleChatGPTResponse)
+    .catch(error => console.error('Error:', error));
 }
 
+// Call the main function to start the process when the webpage is loaded
 processWebpage();
+
