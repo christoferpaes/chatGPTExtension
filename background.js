633399dc7e2570bf01a6b9805ad3740d7d0edf3f
Christofer Patrick Paes <49449786+christoferpaes@users.noreply.github.com>
2023-06-22 12:22:26 -0700

    Create background.js

---
+++ background.js
@@ -0,0 +1,34 @@
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.message === "math_equation") {
+    sendToChatGPT(request.equation, function(response) {
+      sendResponse({ message: "chat_response", response: response });
+    });
+    return true;
+  }
+});
+
+function sendToChatGPT(equation, callback) {
+  const apiKey = "YOUR_OPENAI_API_KEY"; // replace with your API key 
+  const apiUrl = "https://api.openai.com/v1/engines/davinci-codex/completions";
+
+  fetch(apiUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${apiKey}`
+    },
+    body: JSON.stringify({
+      prompt: equation,
+      max_tokens: 50
+    })
+  })
+    .then(response => response.json())
+    .then(data => {
+      const chatResponse = data.choices[0].text.trim();
+      callback(chatResponse);
+    })
+    .catch(error => {
+      console.error("Error:", error);
+      callback("An error occurred while processing the request.");
+    });
+}
